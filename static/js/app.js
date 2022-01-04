11a7e27334d15352663205aac9abd992cca65deb
merdi-akelax <akelaxmerdi@gmail.com>
2022-01-04 14:12:04 +0100

    add: html content

--- static/js/app.js
+++ static/js/app.js
@@ -1,7 +1,8 @@
 document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.datepicker');
-    var options = {};
-    var instances = M.Datepicker.init(elems, options);
+  var elems = document.querySelectorAll('.datepicker');
+  var options = {};
+  var instances = M.Datepicker.init(elems, options);
+  var modalElems = document.querySelectorAll('.modal');
+  var modal_instances = M.Modal.init(modalElems);
 });
-
 /* #26a69a 1976D2 */
