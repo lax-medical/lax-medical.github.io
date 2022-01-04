e4e3f177ea42ff2fee985a2fc4aa91d861b01f05
merdi-akelax <akelaxmerdi@gmail.com>
2022-01-04 11:57:34 +0100

    first commit

---
+++ static/js/app.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.datepicker');
+    var options = {};
+    var instances = M.Datepicker.init(elems, options);
+});
+
+/* #26a69a 1976D2 */
